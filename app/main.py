229c1684170e47fc00f69147b1fa172178ce645f
scp10124 <aa0970543252@gmail.com>
2024-12-19 15:17:53 +0800

    A student work

--- app/main.py
+++ app/main.py
@@ -1,19 +1,12 @@
 from fastapi import FastAPI, Depends, HTTPException
-from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-
 from .database import Base, engine, SessionLocal
-from .models import Todo
-from .schemas import TodoBase, TodoCreate, TodoResponse
 from .routers import router
 
 #Initialize FastAPI
 app = FastAPI()
 
 # Create database tables
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 #Register Router
 app.include_router(router=router, prefix="/api",tags=["Todos"])
